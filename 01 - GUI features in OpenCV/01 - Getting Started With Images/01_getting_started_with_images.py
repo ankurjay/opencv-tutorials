3f28a26870ddf70c4a3ef756f8dfaf203b80d186
ankurjay <ankur.jay007@gmail.com>
2025-11-12 19:48:42 -0500

    Add first tutorial py script and image

---
+++ 01 - GUI features in OpenCV/01 - Getting Started With Images/01_getting_started_with_images.py
@@ -0,0 +1,25 @@
+import sys
+
+# Import the OpenCV Library
+import cv2 as cv
+
+# Read the image
+img = cv.imread("data/starry_night.jpg")
+
+
+# Check if the image was loaded correctly
+if img is None:
+    sys.exit("Could not read the image!")
+
+
+# Display the image. "Display Window" is the window title.
+cv.imshow("Display Window", img)
+
+
+# Display the image until a key is pressed, or the timer (milliseconds)
+# runs out. '0' means wait indefinitely for a key press.
+k = cv.waitKey(0)
+
+# If the key pressed was 's', save the image.
+if k == ord("s"):
+    cv.imwrite("/tmp/starry_night.png", img)
